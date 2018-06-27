8f3974c8dfc05c9b5724048105dced2134ce25e8
Carolina Lastra <carolastra.cofre@gmail.com>
2018-06-27 18:05:35 -0400

    trans-lab menu.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-/* Holiii acá va tu código también */
+const inputEmail = document.getElementById('exampleInputEmail1').value;
+const inputPassword = document.getElementById('exampleInputPassword1').value;
+const btn = document.getElementById ('btn');
+
+ 
+let validate = () => {
+  if (inputPassword === NaN){
+    inputPassword.innerHTML="Enter numeric value only";
+    return false;
+  }else{
+    return true;
+    }
+  }
