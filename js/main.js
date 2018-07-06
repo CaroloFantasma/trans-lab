4009c33a119fd72773db80f43832180df6e8b77d
Carolina Lastra <carolastra.cofre@gmail.com>
2018-07-05 21:04:27 -0400

    más tarjetas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const inputEmail = document.getElementById('exampleInputEmail1');
 const inputPassword = document.getElementById('InputPassword').value;
 const btn = document.getElementById ('btn');
- 
 
-  btn.addEventListener('click', () => {
-    if (inputPassword === /^[0-9]+$/ || inputPassword.length <= 8 ) {
-    window.open('menu.html');
-    } else {
-    document.getElementById('wrong').innerHTML= 'Excede el número de digitos.';
-    }
-    });
+// Revisar si el correo electrónico es válido
+
+btn.addEventListener('click', () => {
+  if (inputEmail === /^[w-.+]+@[a-zA-Z0-9.-]+.[a-zA-z0-9]{2,4}$/) {
+    if (inputPassword === /^[0-9]+$/ || inputPassword.length >= 8) {
+      window.open('menu.html');
+  } else {
+  alert("Ingrese email válido");
+  }
+}
+  })
+ 
